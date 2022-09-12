fa3f4c0b76e5ad4402cc4a609516e8fc5e38b168
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-09-12 19:27:06 -0300

    testing requests
    using response.JSON

--- esports/backend/src/server.mjs
+++ esports/backend/src/server.mjs
@@ -3,7 +3,12 @@ import express from "express";
 const app = express();
 
 app.get("/ads", (request, response) => {
-  return response.send('Acessou a rota ads')
+  return response.json([
+    {id: 1, name: "Ad 1"},
+    {id: 2, name: "Ad 2"},
+    {id: 3, name: "Ad 3"},
+    {id: 4, name: "Ad 4"},
+  ])
 })
 
 app.listen(3333) //aplicação fica ouvindo a porta 3333, e não para de rodar até o usuário fechar o terminal
