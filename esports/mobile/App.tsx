2bc8e3b13a8d45cd65928bb21a33c7bfa2d6bfb6
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-09-13 22:24:46 -0300

    import images and create styles.css
    import styles.css in App.tsx

--- esports/mobile/App.tsx
+++ esports/mobile/App.tsx
@@ -1,19 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+  function Button(props: ButtonProps){
+    return (
+        <TouchableOpacity>
+          <Text>
+            {props.title}
+          </Text>
+        </TouchableOpacity>
+    )
+  }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        App by Geovane Rigonato!
+      </Text>
+
+      <Button title="Click me --> 1" />
+      <Button title="Click me --> 2" />
+      <Button title="Click me --> 3" />
+
+      <StatusBar style="auto"/>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- esports/web/src/App.tsx
+++ esports/web/src/App.tsx
@@ -2,25 +2,13 @@
 
 //component --> capitalized function that returns a set of HTML tags
 
-interface ButtonProps {
-title: string;
-}
+import './styles.css'
 
-function Button(props: ButtonProps) {
-return(
-  <button>
-    {props.title}
-  </button>
-  )
-}
+
 function App() {
 return (
   
   <div>
-  <Button title="Send 1"/>
-  <Button title="Send 2"/>
-  <Button title="Send 3"/>
-  <Button title="Hello World"/>
   </div>
 )
 }
