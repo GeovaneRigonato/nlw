49a838c0abafcce1072b43206cc55367ae18a043
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-09-14 15:23:14 -0300

    create a component loading
    the "Loading" component is used to apply the fonts, if they haven't been loaded yet, he it will be shown on the screen.

--- esports/mobile/App.tsx
+++ esports/mobile/App.tsx
@@ -1,8 +1,25 @@
 import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter";
 
+import { Home } from './src/screens/Home';
 import { Background } from './src/Components/Background';
+import { Loading } from "./src/Components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  
   return (
     <Background>
       <StatusBar
@@ -10,6 +27,8 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
+
+      {fontsLoaded ? <Home /> : <Loading/>}
     </Background>
   );
 
