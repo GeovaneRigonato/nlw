dbf1e59b0385b5183e0ab763298b3ce564ceba43
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-09-14 17:35:20 -0300

    created a "Header" component

--- esports/mobile/src/screens/Home/index.tsx
+++ esports/mobile/src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { View, Image } from 'react-native';
 
 import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from '../../Components/Heading';
 
 import { styles } from './styles';
 
@@ -11,6 +11,11 @@ export function Home() {
       <Image 
       source={logoImg} 
       style={styles.logo}
+      />
+
+      <Heading
+      title="Encontre seu duo!"
+      subtitle="Selecione o game que deseja jogar..."
       />
 
     </View>
