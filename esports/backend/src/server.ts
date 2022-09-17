61d85046ac78c8388c0d0ccd7a0b43397e46e50d
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-09-17 11:15:42 -0300

    created table game and Ad

--- esports/backend/src/server.ts
+++ esports/backend/src/server.ts
@@ -1,15 +1,42 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+/**
+ * Query: 
+ * Route:
+ * Body:
+ */
+
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  const gameId = request.params.id;
+
+  return response.send(gameId);
+
   return response.json([
-    {id: 1, name: "Ad 1"},
-    {id: 2, name: "Ad 2"},
-    {id: 3, name: "Ad 3"},
+    { id: 1, name: "Ad 1" },
+    { id: 2, name: "Ad 2" },
+    { id: 3, name: "Ad 3" },
+    { id: 4, name: "Ad 4" },
+    { id: 5, name: "Ad 5" },
   ])
+})
+
+
+app.get("/ads/:id/discord", (request, response) => {
+  // const adId = request.params.id;
+
+
+  return response.json([])
 })
 
 app.listen(3333) //aplicação fica ouvindo a porta 3333, e não para de rodar até o usuário fechar o terminal
 
- 
