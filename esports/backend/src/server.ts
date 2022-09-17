5283e349d69b5407f8b6170918fe13a20ba4c0e7
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-09-17 16:57:50 -0300

    create routes

--- esports/backend/src/server.ts
+++ esports/backend/src/server.ts
@@ -1,6 +1,16 @@
 import express, { response } from "express";
-
+import cors from "cors";
+import { PrismaClient } from '@prisma/client';
+import { convertHourStringToMinutes } from "./utils/convert-hour-string-to-minutes";
+import { convertMinutesToHourString } from "./utils/convert-minutes-to-hour-string";
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const prisma = new PrismaClient({
+  log: ['query']
+});
 
 /**
  * Query: 
@@ -8,34 +18,88 @@ const app = express();
  * Body:
  */
 
-app.get("/games", (request, response) => {
-  return response.json([]);
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        }
+      }
+    }
+  })
+
+  return response.json([games]);
 });
 
-app.post("/ads", (request, response) => {
-  return response.status(201).json([]);
+app.post("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+  const body = request.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    }
+  });
+
+
+  return response.status(201).json([ad]);
 });
 
-app.get("/games/:id/ads", (request, response) => {
+app.get("/games/:id/ads", async (request, response) => {
   const gameId = request.params.id;
 
-  return response.send(gameId);
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
 
-  return response.json([
-    { id: 1, name: "Ad 1" },
-    { id: 2, name: "Ad 2" },
-    { id: 3, name: "Ad 3" },
-    { id: 4, name: "Ad 4" },
-    { id: 5, name: "Ad 5" },
-  ])
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd),
+    }
+  }))
 })
 
 
-app.get("/ads/:id/discord", (request, response) => {
-  // const adId = request.params.id;
+app.get("/ads/:id/discord", async (request, response) => {
+  const adId = request.params.id;
 
+  const ad = await prisma.ad.findUniqueOrThrow({
+    where: {
+      id: adId,
+    },
+    select: {
+      discord: true,
+    }
+  });
 
-  return response.json([])
+  return response.json([{
+    discord: ad.discord,
+  }])
 })
 
 app.listen(3333) //aplicação fica ouvindo a porta 3333, e não para de rodar até o usuário fechar o terminal
