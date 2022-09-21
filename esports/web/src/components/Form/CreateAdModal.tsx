82e99c505ea4ed5c7d77a78daa81389882000716
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-09-20 23:49:10 -0300

    Select using finished Radix (challenge)

--- esports/web/src/components/Form/CreateAdModal.tsx
+++ esports/web/src/components/Form/CreateAdModal.tsx
@@ -2,18 +2,26 @@ import * as Dialog from "@radix-ui/react-dialog";
 import * as CheckBox from "@radix-ui/react-checkbox";
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
 
-import { CaretDown, Check, GameController } from "phosphor-react";
+
+import { Check, GameController } from "phosphor-react";
 
 import { Input } from "./Input";
 import { FormEvent, useEffect, useState } from "react";
 
 import axios from 'axios';
 
+
+import * as SelectPrimitive from '@radix-ui/react-select';
+import { CheckIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+import { blackA, mauve, violet } from "@radix-ui/colors";
+import { styled } from "@stitches/react";
+
 interface Game {
     id: string;
     title: string;
 
 }
+
 
 export function CreateAdModal() {
     const [games, setGames] = useState<Game[]>([])
@@ -31,6 +39,8 @@ export function CreateAdModal() {
 
         const formData = new FormData(event.target as HTMLFormElement)
         const data = Object.fromEntries(formData)
+
+
 
 
         if (!data.game) {
@@ -67,35 +77,49 @@ export function CreateAdModal() {
     }
 
 
-
     return (
         <Dialog.Portal>
             <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
             <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
                 <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
-                <form onSubmit={handleCreateAd} className="mt-8 flex flex-col gap-4">
+                <form onSubmit={handleCreateAd} className="mt-8 flex flex-col gap-4 ">
 
                     <div className="flex flex-col gap-2">
                         <label htmlFor="game" className="font-semibold">Qual o game?</label>
 
-                        <select
-                            name="game"
-                            className="bg-zinc-900 py-3 px-4 rouded text-sm placeholder:text-zinc-500 appearance-none"
-                            id="game"
-                            defaultValue=""
-                        >
-                            <option disabled selected value="">Selecione o game que deseja jogar</option>
-                            {games.map(game => {
-                                return <option key={game.id} value={game.id}>{game.title}</option>
-                            }
-                            )}
+                        <Box >
+                            <Select name="game" >
+                                <SelectTrigger className='text-sm  px-4 appearance-none bg-zinc-900 w-[368px] flex flex-row justify-between items-center' aria-label="game">
+                                    <SelectValue placeholder="Selecione o game que deseja jogar" />
+                                    <SelectIcon>
+                                        <ChevronDownIcon className='w-5 h-5 text-zinc-500' />
+                                    </SelectIcon>
+                                </SelectTrigger>
+                                <SelectContent >
+                                    <SelectViewport>
+                                        <SelectGroup>
+                                            {games.map(game => {
+                                                return (
+                                                    <SelectItem className="text-zinc-300" key={game.id} value={game.id}>
+                                                        <SelectItemText>{game.title}</SelectItemText>
+                                                        <SelectItemIndicator>
+                                                            <CheckIcon />
+                                                        </SelectItemIndicator>
+                                                    </SelectItem>
+                                                )
+                                            }
+                                            )}
+                                        </SelectGroup>
+                                    </SelectViewport>
+                                </SelectContent>
+                            </Select>
+                        </Box>
 
-                        </select>
 
                     </div>
 
-                    <div className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-2 ">
                         <label htmlFor="name">Seu nome (ou nickname)</label>
                         <Input name="name" id="name" placeholder="Como te chamam dento do game" />
                     </div>
@@ -103,7 +127,7 @@ export function CreateAdModal() {
                     <div className="grid grid-cols-2 gap-6">
                         <div className="flex flex-col gap-2">
                             <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-                            <Input name="yearsPlaying" id="yearsPlaying" type="number" placeholder="Tudo bem ser ZERO" />
+                            <Input className="appearance-none" name="yearsPlaying" id="yearsPlaying" type="number" placeholder="Tudo bem ser ZERO" />
                         </div>
 
                         <div className="flex flex-col gap-2">
@@ -222,4 +246,126 @@ export function CreateAdModal() {
         </Dialog.Portal>
     )
 }
+
+const StyledTrigger = styled(SelectPrimitive.SelectTrigger, {
+    all: 'unset',
+    borderRadius: 4,
+    fontSize: 13,
+    lineHeight: 1,
+    height: 44,
+    gap: 5,
+    backgroundColor: '#18181B',
+    boxShadow: `0 2px 10px ${blackA.blackA7}`,
+    '&:hover': { backgroundColor: '#18181B' },
+    '&:focus': { boxShadow: `0 0 0 2px black` },
+    '&[data-placeholder]': { color: '#71717A' },
+    cursor: 'pointer',
+});
+
+const StyledIcon = styled(SelectPrimitive.SelectIcon, {
+    all: 'unset',
+});
+
+const StyledContent = styled(SelectPrimitive.Content, {
+    overflow: 'hidden',
+    backgroundColor: '#18181B',
+    borderRadius: 6,
+});
+
+const StyledViewport = styled(SelectPrimitive.Viewport, {
+    padding: 5,
+});
+
+interface Props {
+    children?: React.ReactNode;
+    // any props that come into the component
+}
+
+function Content({ children, ...props }: Props) {
+    return (
+        <SelectPrimitive.Portal>
+            <StyledContent {...props}>{children}</StyledContent>
+        </SelectPrimitive.Portal>
+    );
+}
+
+const StyledItem = styled(SelectPrimitive.Item, { //tela de seleção
+    all: 'unset',
+    fontSize: 14,
+    lineHeight: 1,
+    color: '#71717A',
+    borderRadius: 3,
+    display: 'flex',
+    alignItems: 'center',
+    height: 25,
+    width: 350,
+    padding: '0 35px 0 25px',
+    position: 'relative',
+    userSelect: 'none',
+
+    '&[data-disabled]': {
+        color: '#18181B',
+        pointerEvents: 'none',
+    },
+
+    '&[data-highlighted]': {
+        backgroundColor: '#71717A',
+        color: "#18181B",
+    },
+    cursor: 'pointer',
+});
+
+const StyledLabel = styled(SelectPrimitive.Label, {
+    padding: '0 25px',
+    fontSize: 12,
+    lineHeight: '25px',
+    color: "#18181B",
+});
+
+const StyledSeparator = styled(SelectPrimitive.Separator, {
+    height: 1,
+    backgroundColor: violet.violet6,
+    margin: 5,
+});
+
+const StyledItemIndicator = styled(SelectPrimitive.ItemIndicator, {
+    position: 'absolute',
+    left: 0,
+    width: 25,
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+});
+
+const scrollButtonStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 25,
+    backgroundColor: '#18181B',
+    color: violet.violet11,
+    cursor: 'default',
+};
+
+const StyledScrollUpButton = styled(SelectPrimitive.ScrollUpButton, scrollButtonStyles);
+
+const StyledScrollDownButton = styled(SelectPrimitive.ScrollDownButton, scrollButtonStyles);
+
+// Exports
+export const Select = SelectPrimitive.Root;
+export const SelectTrigger = StyledTrigger;
+export const SelectValue = SelectPrimitive.Value;
+export const SelectIcon = StyledIcon;
+export const SelectContent = Content;
+export const SelectViewport = StyledViewport;
+export const SelectGroup = SelectPrimitive.Group;
+export const SelectItem = StyledItem;
+export const SelectItemText = SelectPrimitive.ItemText;
+export const SelectItemIndicator = StyledItemIndicator;
+export const SelectLabel = StyledLabel;
+export const SelectSeparator = StyledSeparator;
+export const SelectScrollUpButton = StyledScrollUpButton;
+export const SelectScrollDownButton = StyledScrollDownButton;
+
+const Box = styled('div', {});
 
